8315d67443857376a874fb152918305f04d2c7a1
Victoria <dr.v.orlova@gmail.com>
2025-06-28 13:27:57 +0300

    init: start project

---
+++ src/app/layout.tsx
@@ -0,0 +1,33 @@
+import type {Metadata} from 'next'
+import {Geist, Geist_Mono} from 'next/font/google'
+import './globals.css'
+
+const geistSans = Geist({
+  variable: '--font-geist-sans',
+  subsets: ['latin'],
+})
+
+const geistMono = Geist_Mono({
+  variable: '--font-geist-mono',
+  subsets: ['latin'],
+})
+
+export const metadata: Metadata = {
+  title: 'BookStore - Онлайн магазин книг',
+  description:
+    'Найдите и купите любимые книги в нашем онлайн книжном магазине. Широкий выбор художественной и научной литературы, новинки и бестселлеры.',
+}
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+        <main>{children}</main>
+      </body>
+    </html>
+  )
+}
