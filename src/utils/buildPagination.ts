f22b3f8eead8c1683549fbb5d7ceaed400b6d659
Victoria <dr.v.orlova@gmail.com>
2025-07-09 20:54:28 +0300

    fix(pagination): fix type error in buildPaginationScheme

--- src/utils/buildPagination.ts
+++ src/utils/buildPagination.ts
@@ -7,9 +7,21 @@ export const buildSchemePagination = (currentPage: number, pageCount: number) =>
   const scheme: number[] = [1, prevPageNumber, +currentPage, nextPageNumber, pageCount]
   const filteredScheme = scheme.filter(item => item > 0 && item <= pageCount)
   const set = new Set(filteredScheme)
-  const result = Array.from(set)
+  const result: (number | string)[] = Array.from(set)
 
-  if (result[0] + 1 !== result[1]) result.splice(1, 0, '...')
-  if (result.at(-2) + 1 !== result.at(-1)) result.splice(result.length - 1, 0, '...')
+  if (
+    typeof result[0] === 'number' &&
+    typeof result[1] === 'number' &&
+    result[0] + 1 !== result[1]
+  ) {
+    result.splice(1, 0, '...')
+  }
+
+  const secondLast = result.at(-2)
+  const last = result.at(-1)
+  if (typeof secondLast === 'number' && typeof last === 'number' && secondLast + 1 !== last) {
+    result.splice(result.length - 1, 0, '...')
+  }
+
   return result
 }
